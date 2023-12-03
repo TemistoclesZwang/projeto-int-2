13c5de3350a8a1f121adceb7f5514c4a41f79ec7
Temistocles Zwang <temis2st@gmail.com>
2023-12-02 23:33:18 -0300

    fix: counterBtn

--- front-ta-pronto/src/pages/Menu/CounterBtn.tsx
+++ front-ta-pronto/src/pages/Menu/CounterBtn.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useOrderListContext } from '../../context/OrderListContext';
 
 interface CounterProps {
@@ -8,34 +8,50 @@ interface CounterProps {
 
 export function CounterBtn({ id, name }: CounterProps): JSX.Element {
   const { orders, setOrders } = useOrderListContext();
-  const order = orders.find((order) => order.id === id);
+  const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    // Atualiza o count quando a lista de orders for alterada
+    const itemCount = orders.filter((order) => order.id === id).length;
+    setCount(itemCount);
+  }, [orders, id]);
 
   const handleIncrement = () => {
-    if (order) {
-      const updatedOrders = orders.map((o) =>
-        o.id === id ? { ...o, count: (o.count || 0) + 1 } : o
-      );
-      setOrders(updatedOrders);
-    } else {
-      setOrders([...orders, { id, name, count: 1 }]);
-    }
+      setOrders([...orders, { id, name }]);
+      // Atualiza o count utilizando o estado atualizado diretamente
+      setCount(count + 1);
+    
   };
 
   const handleDecrement = () => {
-    if (order && order.count && order.count > 0) {
-      const updatedOrders = orders.map((o) =>
-        o.id === id ? { ...o, count: (o.count || 0) - 1 } : o
-      );
-      setOrders(updatedOrders.filter((o) => o.count !== 0));
+    const existingOrderIndex = orders.findIndex((order) => order.id === id);
+
+    if (existingOrderIndex !== -1) {
+      // Remove o pedido específico da lista
+      const updatedOrders = [...orders];
+      updatedOrders.splice(existingOrderIndex, 1);
+      setOrders(updatedOrders);
+
+      // Atualiza o count após a remoção do pedido
+      const itemCount = updatedOrders.filter((order) => order.id === id).length;
+      setCount(itemCount);
     }
   };
 
+  const handleClearAllOrders = () => {
+    // Limpa todas as orders
+    setOrders([]);
+    // Atualiza o count para 0
+    setCount(0);
+  };
+
   return (
-    <div>
-      <h2>{name}</h2>
+    <div className='counterBtnContainer'>
+      <p>{name}</p>
       <button onClick={handleIncrement}>+</button>
-      <span>{order ? order.count : 0}</span>
+      <span>{count}</span>
       <button onClick={handleDecrement}>-</button>
+      <button onClick={handleClearAllOrders}>Limpar Orders</button>
     </div>
   );
 }
